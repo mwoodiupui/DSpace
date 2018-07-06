69487b307454616bfa02655e231467785d964943
Mark H. Wood <mwood@IUPUI.Edu>
2018-07-06 12:45:28 -0400

    [DS-3453] Manual port of master PR #1683 to 7_x

--- dspace-api/src/main/java/org/dspace/content/dao/impl/MetadataValueDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/MetadataValueDAOImpl.java
@@ -22,19 +22,21 @@ import org.dspace.content.MetadataValue;
 import org.dspace.content.dao.MetadataValueDAO;
 import org.dspace.core.AbstractHibernateDAO;
 import org.dspace.core.Context;
+import org.dspace.storage.rdbms.DatabaseUtils;
 
 /**
- * Hibernate implementation of the Database Access Object interface class for the MetadataValue object.
- * This class is responsible for all database calls for the MetadataValue object and is autowired by spring
+ * Hibernate implementation of the Database Access Object interface class for
+ * the MetadataValue object.  This class is responsible for all database calls
+ * for the MetadataValue object and is autowired by Spring.
  * This class should never be accessed directly.
  *
  * @author kevinvandevelde at atmire.com
+ * @author Adán Román Ruiz at arvo.es (DS-3453)
  */
 public class MetadataValueDAOImpl extends AbstractHibernateDAO<MetadataValue> implements MetadataValueDAO {
     protected MetadataValueDAOImpl() {
         super();
     }
-
 
     @Override
     public List<MetadataValue> findByField(Context context, MetadataField metadataField) throws SQLException {
@@ -82,9 +84,18 @@ public class MetadataValueDAOImpl extends AbstractHibernateDAO<MetadataValue> im
 
     @Override
     public MetadataValue getMinimum(Context context, int metadataFieldId)
-        throws SQLException {
-        String queryString = "SELECT m FROM MetadataValue m JOIN FETCH m.metadataField WHERE m.metadataField.id = " +
-            ":metadata_field_id ORDER BY text_value";
+            throws SQLException {
+        String queryString;
+        if (context.getDbType().equals(DatabaseUtils.DBMS_ORACLE)) {
+            queryString = "SELECT m FROM MetadataValue m JOIN FETCH m.metadataField"
+                    + " WHERE m.metadataField.id = :metadata_field_id"
+                    + " ORDER BY DBMS_LOB.substr(text_value , 4000 , 1)";
+        } else {
+            queryString = "SELECT m FROM MetadataValue m JOIN FETCH m.metadataField"
+                    + " WHERE m.metadataField.id = :metadata_field_id"
+                    + " ORDER BY text_value";
+        }
+
         Query query = createQuery(context, queryString);
         query.setParameter("metadata_field_id", metadataFieldId);
         query.setMaxResults(1);
@@ -95,5 +106,4 @@ public class MetadataValueDAOImpl extends AbstractHibernateDAO<MetadataValue> im
     public int countRows(Context context) throws SQLException {
         return count(createQuery(context, "SELECT count(*) FROM MetadataValue"));
     }
-
 }
