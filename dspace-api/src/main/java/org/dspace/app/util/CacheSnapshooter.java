4ac1eaf12d81d6f59672759a33af54279bc62af6
Mark H. Wood <mhwood@Ameritech.Net>
2019-04-11 09:51:37 -0400

    Tidy, document, flesh out with actual statistics.

--- dspace-api/src/main/java/org/dspace/app/util/CacheSnapshooter.java
+++ dspace-api/src/main/java/org/dspace/app/util/CacheSnapshooter.java
@@ -1,26 +1,24 @@
-/*
- * Copyright 2019 Indiana University.  All rights reserved.
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
  *
- * Mark H. Wood, IUPUI University Library, Apr 9, 2019
+ * http://www.dspace.org/license/
  */
-
-/*
- * Copyright 2019 Indiana University.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.dspace.app.util;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.TimeZone;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -31,20 +29,25 @@ import net.sf.ehcache.statistics.StatisticsGateway;
 
 /**
  * Periodically sample the statistics of all caches.
+ * This is a Bean.  Configure it in Spring to use it.  Samples are written to a
+ * CSV OutputStream which is injected by the container.  Samples are taken at
+ * intervals the length of which is injected by the container.
  *
  * @author Mark H. Wood <mwood@iupui.edu>
  */
 @Named
 public class CacheSnapshooter {
-    /** Take samples this many seconds apart. */
+    /** Take samples this many seconds apart. If less than or equal to 0, do nothing.  Required. */
     @Inject
     private Integer interval;
 
-    /** Write samples in CSV to this file. */
+    /** Write samples in CSV to this stream.  Required. */
     @Inject
     private OutputStream output;
 
-    public CacheSnapshooter() {
+    /** Set up the timer and task, and start them. */
+    @PostConstruct
+    private void init() {
         if (interval > 0) {
             ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
             ScheduledFuture<?> future
@@ -53,34 +56,63 @@ public class CacheSnapshooter {
         }
     }
 
-    private static class Sampler implements Runnable {
+    /**
+     * Task to sample the cache statistics and write them out.
+     */
+    private static class Sampler
+            implements Runnable {
+        /** CSV column header labels */
+        private static final String[] HEADER = {
+            "time",
+            "manager",
+            "cache",
+            "hitRatio",
+            "hitCount",
+            "putCount",
+            "evictedCount",
+        };
+
         private final CSVWriter writer;
         private final SimpleDateFormat dateFormat;
-        private static final String[] header = {"time", "manager", "cache", };
+        private final String[] nextLine = new String[HEADER.length];
 
+        /**
+         * Prepare for writing, and write the CSV column headers.
+         * @param output all output will be written here.
+         */
         private Sampler(OutputStream output) {
             writer = new CSVWriter(new OutputStreamWriter(output));
             dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
             dateFormat.setTimeZone(TimeZone.getTimeZone(ZoneOffset.UTC));
-            writer.writeNext(header);
+            writer.writeNext(HEADER);
         }
 
         @Override
         public void run() {
-            String now = dateFormat.format(new Date());
-            String[] nextLine = new String[header.length];
-            nextLine[0] = now;
+            nextLine[0] = dateFormat.format(new Date());
             for (CacheManager manager : CacheManager.ALL_CACHE_MANAGERS) {
                 nextLine[1] = manager.getName();
                 for (String cacheName : manager.getCacheNames()) {
                     nextLine[2] = cacheName;
                     Cache cache = manager.getCache(cacheName);
                     StatisticsGateway statistics = cache.getStatistics();
-                    // Write a statistics line TODO with statistics
+                    nextLine[3] = String.valueOf(statistics.cacheHitRatio());
+                    nextLine[4] = String.valueOf(statistics.cacheHitCount());
+                    nextLine[5] = String.valueOf(statistics.cachePutCount());
+                    nextLine[6] = String.valueOf(statistics.cacheEvictedCount());
                     writer.writeNext(nextLine);
                 }
             }
             writer.flushQuietly();
+        }
+
+        /**
+         * When the container is shutting down, close the CSV stream.
+         */
+        @PreDestroy
+        private void close()
+                throws IOException {
+            writer.close();
         }
     }
 }
